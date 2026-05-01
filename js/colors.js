d4a71c3ee9360537b7330b1cc585e03bca5b5c60
tomer.rrr <tomer.rrr@gmail.com>
2026-05-01 07:39:56 +0000

    תיקון באג חישוב hexToRgb

--- js/colors.js
+++ js/colors.js
@@ -34,8 +34,13 @@ export function adjustBrightness(hex, factor) {
 }
 
 export function hexToRgb(hex) {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : [0, 0, 0];
+    // מניחים שהפורמט הוא תמיד #RRGGBB
+    const rgb = parseInt(hex.substring(1), 16);
+    return [
+        (rgb >> 16) & 0xff, // R
+        (rgb >> 8) & 0xff,  // G
+        (rgb >> 0) & 0xff   // B
+    ];
 }
 
 export function lerp(start, end, amt) {
